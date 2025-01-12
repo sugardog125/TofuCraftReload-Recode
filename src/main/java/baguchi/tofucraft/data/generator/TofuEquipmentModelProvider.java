2a49f069c7798d57b0f57069f837492420e49b4e
baguchan <shutux500@gmail.com>
2025-01-12 17:56:40 +0900

    1.21.4-14.4.2.0

--- src/main/java/baguchi/tofucraft/data/generator/TofuEquipmentModelProvider.java
+++ src/main/java/baguchi/tofucraft/data/generator/TofuEquipmentModelProvider.java
@@ -15,7 +15,7 @@ public class TofuEquipmentModelProvider implements DataProvider {
 	private final PackOutput.PathProvider pathProvider;
 
 	public TofuEquipmentModelProvider(PackOutput p_371200_) {
-		this.pathProvider = p_371200_.createPathProvider(PackOutput.Target.RESOURCE_PACK, "models/equipment");
+		this.pathProvider = p_371200_.createPathProvider(PackOutput.Target.RESOURCE_PACK, "equipment");
 	}
 
 	@Override
